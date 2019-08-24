928be2841bb22fff8a29195950b290787f590707
Nishant-Adhikari <adhikari.nishant@gmail.com>
2019-08-24 01:14:01 -0500

    Updates

---
+++ precache-manifest.a0829688bcb78b9e3084ac7642768b02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0066603d94ebe6c27d2ca0b6d84e25b7",
+    "url": "/socialgram/index.html"
+  },
+  {
+    "revision": "b01cf0944f3e994b3f92",
+    "url": "/socialgram/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e1632d7e8607eb384eae",
+    "url": "/socialgram/static/js/2.d2b3733c.chunk.js"
+  },
+  {
+    "revision": "b01cf0944f3e994b3f92",
+    "url": "/socialgram/static/js/main.6a41c332.chunk.js"
+  },
+  {
+    "revision": "1f5d6b6a0305d233be68",
+    "url": "/socialgram/static/js/runtime~main.e9f44b11.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/socialgram/static/media/logo.5d5d9eef.svg"
+  }
+]);
